f47a09752b12815fa300924a9b3929265478c752
KhadijaFatimaa <110911091+KhadijaFatimaa@users.noreply.github.com>
2022-08-12 20:31:02 -0500

    Update exercise.py

--- assignments/01Peso/src/exercise.py
+++ assignments/01Peso/src/exercise.py
@@ -1,8 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
     print('Hola')
+currentweight = float(input("Input your current weight: "))
 
+idealweight = float(input("Input your ideal weight: "))
 
+DietTimeframe = float(input("For how many months will you be in the program?: "))
 
+print("You will need to lose: ",((currentweight - idealweight)/DietTimeframe), "Kilograms per month")
 if __name__ == '__main__':
     main()
