0be4715474c259981c790bb1490b7cb117d650a5
KhadijaFatimaa <110911091+KhadijaFatimaa@users.noreply.github.com>
2022-08-16 07:41:04 -0500

    Update exercise.py

--- assignments/03DistanciaDosPuntos/src/exercise.py
+++ assignments/03DistanciaDosPuntos/src/exercise.py
@@ -1,13 +1,13 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
-x1 = float(input('x1= '))
-y1 = float(input('y1= '))
-x2 = float(input('x2= '))
-y2 = float(input('y2= '))
+x1=float(input('x1= '))
+y1=float(input('y1= '))
+x2=float(input('x2= '))
+y2=float(input('y2= '))
 
-distancia = ((x2-x1)**2/(y2-y1)**2)**0.5
-print(f'**distancia= {pre_round: .2f}')
+distancia1=(((x2-x1)**2+((y2-y1)**2)**(1/2))
+print(f'distancia= {distancia1:.2f}')
 
 
 if __name__ == '__main__':
